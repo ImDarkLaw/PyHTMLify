fed9430adc7c255e32377c7a0fa573046b60aff4
ImDarkLaw <alkavalieratos@gmail.com>
2024-03-16 15:56:25 +0200

    Update ui.py

--- src/ui.py
+++ src/ui.py
@@ -5,7 +5,7 @@ from tkinter import filedialog, messagebox
 from converter import convert_markdown_to_html
 
 customtkinter.set_appearance_mode("dark")
-customtkinter.set_default_color_theme("dark-blue")
+customtkinter.set_default_color_theme("green")
 
 
 class PyHTMLifyInterface:
@@ -13,43 +13,43 @@ class PyHTMLifyInterface:
         self.root = root_window
         root_window.title("PyHTMLify")
         root_window.maxsize(1600, 1300)
-        root_window.minsize(600, 600)
+        root_window.minsize(600, 700)
 
-        # Calculate the center coordinates of the screen
+        """
+        Center window calculation & logic
+        """
         screen_width = root_window.winfo_screenwidth()
         screen_height = root_window.winfo_screenheight()
-        window_width = 1200
-        window_height = 850
+        window_width = 1400
+        window_height = 1000
         x_coordinate = (screen_width - window_width) // 2
         y_coordinate = (screen_height - window_height) // 2
-
-        # Set the geometry of the window to center it on the screen
         root_window.geometry(f"{window_width}x{window_height}+{x_coordinate}+{y_coordinate}")
-
-        # The initial size the window uses (width x height)
-        # root.geometry("1200x900")
 
         frame = customtkinter.CTkFrame(master=root,  border_color="#FFCC70", border_width=2)  # fg_color="#8D6F3A",
         frame.pack(fill="both", expand=True)
-        # fill="both", expand=True
 
         """
         Widgets https://customtkinter.tomschimansky.com/documentation/widgets
         """
 
-        title_label = customtkinter.CTkLabel(master=frame, text="Paste Markdown content into the text box", font=('Arial', 16))
-        title_label.pack(pady=5)
+        title_label = customtkinter.CTkLabel(master=frame, text="Paste Markdown content into the text box", font=('Arial', 24))
+        title_label.pack(pady=20)
 
-        convert_button = customtkinter.CTkButton(master=frame, text="Convert", command=self.convert_handler, corner_radius=32, border_color="#FFCC70", border_width=2, fg_color="transparent")
+        convert_button = customtkinter.CTkButton(master=frame, text="Convert", command=self.convert_handler,
+        corner_radius=32, border_color="#FFCC70", border_width=2, fg_color="transparent")
         convert_button.pack(pady=10)
 
-        export_button = customtkinter.CTkButton(master=frame, text="Export", command=self.export_handler, corner_radius=32, border_color="#FFCC70", border_width=2, fg_color="transparent")
+        export_button = customtkinter.CTkButton(master=frame, text="Export", command=self.export_handler,
+        corner_radius=32, border_color="#FFCC70", border_width=2, fg_color="transparent")
         export_button.pack(pady=10)
 
-        self.input_text = customtkinter.CTkTextbox(master=frame, width=600, height=300, font=('Arial', 12), corner_radius=16, border_color="#FFCC70", border_width=2)
-        self.input_text.pack(padx=10, pady=10)
+        self.input_text = customtkinter.CTkTextbox(master=frame, width=700, height=300, font=('Arial', 18), corner_radius=16,
+        border_color="#FFCC70", border_width=2, scrollbar_button_color="#FFE4B3", scrollbar_button_hover_color="#FFCC70")
+        self.input_text.pack(padx=10, pady=30)
 
-        self.output_text = customtkinter.CTkTextbox(master=frame, width=600, height=300, font=('Arial', 12), corner_radius=16, border_color="#FFCC70", border_width=2, state='disabled')
+        self.output_text = customtkinter.CTkTextbox(master=frame, width=700, height=300, font=('Arial', 18), corner_radius=16,
+        border_color="#FFCC70", border_width=2, scrollbar_button_color="#FFE4B3", scrollbar_button_hover_color="#FFCC70", state='disabled')
         self.output_text.pack(padx=10, pady=30)
 
     def convert_handler(self):
