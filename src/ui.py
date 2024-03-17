ec57be4b47ebddfa8aaedaa19254c10114e84e64
ImDarkLaw <alkavalieratos@gmail.com>
2024-03-17 23:45:49 +0200

    Full rewrite of the geometry manager using the grid system

--- src/ui.py
+++ src/ui.py
@@ -8,52 +8,70 @@ customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("green")
 
 
-class PyHTMLifyInterface:
-    def __init__(self, root_window):
-        self.root = root_window
-        root_window.title("PyHTMLify")
-        root_window.maxsize(1600, 1300)
-        root_window.minsize(600, 700)
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
 
         """
-        Center window calculation & logic
+        Window attributes
         """
-        screen_width = root_window.winfo_screenwidth()
-        screen_height = root_window.winfo_screenheight()
-        window_width = 1200
-        window_height = 900
+
+        self.title("PyHTMLify")
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+        self.maxsize(1600, 900)
+        self.minsize(400, 560)
+
+        """
+        Calculation & logic for window centering
+        """
+
+        screen_width = self.winfo_screenwidth()
+        screen_height = self.winfo_screenheight()
+        window_width = 840
+        window_height = 780
         x_coordinate = (screen_width - window_width) // 2
         y_coordinate = (screen_height - window_height) // 2
-        root_window.geometry(f"{window_width}x{window_height}+{x_coordinate}+{y_coordinate}")
-
-        frame = customtkinter.CTkFrame(master=root, border_width=2)  # fg_color="#8D6F3A", border_color="#BDE9BA",
-        frame.pack(fill="both", expand=True)
+        self.geometry(f"{window_width}x{window_height}+{x_coordinate}+{y_coordinate}")
 
         """
         Widgets https://customtkinter.tomschimansky.com/documentation/widgets
         """
 
-        title_label = customtkinter.CTkLabel(master=frame, text="Paste Markdown content into the text box", font=('Arial', 24))
-        title_label.pack(pady=20)
+        # self.frame = customtkinter.CTkFrame(master=self, border_width=2)
+        # self.frame.grid(row=0, column=0, sticky="nsew")
 
-        convert_button = customtkinter.CTkButton(master=frame, text="Convert", font=('Arial', 16), command=self.convert_handler,
+        self.title = customtkinter.CTkLabel(master=self, text="Paste Markdown content into the text box", font=('Arial', 24))
+        self.title.grid(row=0, column=0, padx=20)
+
+        self.conversion_button = customtkinter.CTkButton(master=self, text="Convert", font=('Arial', 16), command=self.conversion_handler,
         corner_radius=32, border_color="#BDE9BA", border_width=2, fg_color="transparent")
-        convert_button.pack(pady=10)
+        self.conversion_button.grid(row=1, column=0, padx=(0, 180), pady=10)  # padx=2, pady=(0, 10)
 
-        export_button = customtkinter.CTkButton(master=frame, text="Export", font=('Arial', 16), command=self.export_handler,
+        self.export_button = customtkinter.CTkButton(master=self, text="Export", font=('Arial', 16), command=self.export_handler,
         corner_radius=32, border_color="#BDE9BA", border_width=2, fg_color="transparent")
-        export_button.pack(pady=10)
+        self.export_button.grid(row=1, column=0, padx=(180, 0), pady=10)  # padx=2, pady=(0, 10)
 
-        self.input_text = customtkinter.CTkTextbox(master=frame, width=700, height=300, font=('Arial', 18), corner_radius=16,
+        self.input_text = customtkinter.CTkTextbox(master=self, width=800, height=300, font=('Arial', 18), corner_radius=16,  # , width=800, height=300
         border_color="#BDE9BA", border_width=2, scrollbar_button_color="#CEEACC", scrollbar_button_hover_color="#BDE9BA")
-        self.input_text.pack(padx=10, pady=30)
+        self.input_text.grid(row=2, column=0, padx=10, pady=20)
 
-        self.output_text = customtkinter.CTkTextbox(master=frame, width=700, height=300, font=('Arial', 18), corner_radius=16,
+        self.output_text = customtkinter.CTkTextbox(master=self, width=800, height=300, font=('Arial', 18), corner_radius=16,  # , width=800, height=300
         border_color="#BDE9BA", border_width=2, scrollbar_button_color="#CEEACC", scrollbar_button_hover_color="#BDE9BA", state='disabled')
-        self.output_text.pack(padx=10, pady=10)
+        self.output_text.grid(row=3, column=0, padx=10, pady=10)
 
-    def convert_handler(self):
-        # Get the Markdown text from the input Text widget
+    """
+    Event Handlers
+    """
+
+    def conversion_handler(self):
+        """
+        This method is triggered when the "Convert" button is clicked.
+        It retrieves Markdown text from the input text box, converts it to HTML
+        using the convert_markdown_to_html function (from converter.py),
+        and displays the HTML output in the output text box.
+        """
+        # Get the Markdown content from the input textbox widget
         markdown_text = self.input_text.get("0.0", 'end')
 
         # Print statement for debugging
@@ -72,7 +90,13 @@ class PyHTMLifyInterface:
         self.output_text.configure(state="disabled")
 
     def export_handler(self):
-        # Get the HTML content from the output Text widget
+        """
+        This method is triggered when the "Export" button is clicked.
+        It retrieves HTML content from the output text box, prompts the user
+        to choose a file location for saving the HTML content, and writes
+        the HTML content to the selected file.
+        """
+        # Get the HTML content from the output textbox widget
         html_content = self.output_text.get("0.0", 'end')
 
         if html_content.strip():
@@ -92,6 +116,5 @@ class PyHTMLifyInterface:
 
 
 if __name__ == "__main__":
-    root = customtkinter.CTk()
-    app = PyHTMLifyInterface(root)
-    root.mainloop()
+    app = App()
+    app.mainloop()
